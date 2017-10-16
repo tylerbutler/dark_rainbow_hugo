00d7792c45ff386940a582c7d3ac1a471c1f44ea
Tyler Butler <tyler@tylerbutler.com>
2017-10-15 17:34:26 -0700

    Add js to asset pipeline and gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var less = require('gulp-less');
 var path = require('path');
 var cleanCSS = require('gulp-clean-css');
 var concatCss = require('gulp-concat-css');
-
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
 
 gulp.task('default', ['build'], function () {
 	// place code for your default task here
@@ -29,10 +31,29 @@ gulp.task('minify-css', ['build-less'], function () {
 		.pipe(gulp.dest('./static/css/'));
 });
 
-gulp.task('build', ['minify-css'], function () {
+gulp.task('minify-js', ['build-js'], function (cb) {
+	pump([
+			gulp.src('./build/static/js/*.js'),
+			uglify(),
+			gulp.dest('./static/js/')
+		],
+		cb
+	);
+});
+
+gulp.task('build', ['minify-css', 'minify-js'], function () {
 	// return gulp.src('./build/static/css/')
 	// 	.pipe(concatCss("dark_rainbow.css"))
 	// 	.pipe(gulp.dest('./static/css/'));
+});
+
+gulp.task('build-js', function () {
+	return gulp.src([
+			'./shared_assets/lib/jquery/jquery.1.11.0.min.js',
+			'./shared_assets/lib/bigfoot/bigfoot.js'
+		])
+		.pipe(concat('dark_rainbow.js'))
+		.pipe(gulp.dest('./build/static/js/'));
 });
 
 // gulp.task('default', function () {
